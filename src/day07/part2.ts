31af32aa6ff80d27e63662a24448ebf435677462
Zhuo Deng <zhuofdeng@gmail.com>
2023-12-07 23:15:47 -0500

    day07 solutions

---
+++ src/day07/part2.ts
@@ -0,0 +1,28 @@
+// Advent of Code - Day 7 - Part Two
+
+import { CardGame } from "./card-game";
+
+export function part2(input: string): number {
+  let result = 0;
+  const cards = input.split('\n');
+  let cardGames: Map<number, Array<CardGame>> = new Map();
+  let rank = 1;
+  cards.forEach((card) => {
+    const game = new CardGame(card, true);
+    const games = cardGames.get(game.typeRank) || [];
+    games.push(game);
+    cardGames.set(game.typeRank, games);
+  });
+
+
+  cardGames = new Map([...cardGames.entries()].sort());
+  for (const [key, value] of cardGames) {
+    const sorted = value.sort((a, b) => a.compare(b) ? 1 : -1);
+    cardGames.set(key, sorted);
+    sorted.forEach((game) => {
+      result += game.betAmount * rank;
+      rank ++;
+    })
+  }
+  return result;
+}
---
+++ test/day07.test.ts
@@ -0,0 +1,11 @@
+// Advent of Code - Day 7
+
+import { part1, part2 } from '../src/day07';
+
+test('part one test', () => {
+  expect(part1("")).toBe(0);
+});
+
+test('part two test', () => {
+  expect(part2("")).toBe(0);
+});
