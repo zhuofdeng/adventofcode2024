6a6a907d2376a91dfd49b2666ae17188ef601c39
Chuck Deng <zhuofdeng@gmail.com>
2024-12-03 19:49:36 -0500

    day3

--- src/day03/part1.ts
+++ src/day03/part1.ts
@@ -1,39 +1,14 @@
+import { findMultiplicationPairs } from "./utils";
+
 // Advent of Code - Day 3 - Part One
-
-import { Position } from "../types";
-import { getNumber } from "./utils";
-
 export function part1(input: string): number {
   let sum = 0;
-  const symbolPositions = new Array<Position>();
 
-  // go through and find all the symbols and their (x, y)
-  const lines = input.split('\n');
-
-  lines.forEach((line, index) => {
-    for(let i = 0; i < line.length; i++) {
-      const char = line.charAt(i);
-      if(isNaN(parseInt(char)) && char !== '.') {
-        symbolPositions.push({x: index, y: i});
-      }
-    }
-  });
-
-  symbolPositions.forEach((position) => {
-    const numbers: number[] = []
-    if (position.x > 0) {
-      sum += getNumber(lines[position.x-1], position.y-1, numbers);
-      sum += getNumber(lines[position.x-1], position.y, numbers);
-      sum += getNumber(lines[position.x-1], position.y+1, numbers);
-    }
-    if (position.x < lines.length -1) {
-      sum += getNumber(lines[position.x + 1], position.y-1, numbers);
-      sum += getNumber(lines[position.x + 1], position.y, numbers);
-      sum += getNumber(lines[position.x + 1], position.y+1, numbers);
-    }
-    sum += getNumber(lines[position.x], position.y-1, numbers);
-    sum += getNumber(lines[position.x], position.y+1, numbers);
-  });
+  
+  const multiplicationPairs = findMultiplicationPairs(input);
   // once we have all the symbols and their location, we can find the numbers that are adjent.
+  multiplicationPairs.forEach((pair) => {
+    sum += (pair.x * pair.y);
+  });
   return sum;
 }
--- src/day03/utils.ts
+++ src/day03/utils.ts
@@ -1,30 +1,48 @@
-export const getNumber = (line: string, index: number, existingNumbers: number[]): number => {
-    if (!isNaN(parseInt(line.charAt(index)))) {
-      // this is a number, but we have to find the whole number.
-      // go left until we reach a symbol
-      let left = index;
-      let right = index;
-      for(left; left >= 0; left--) {
-        if (isNaN(parseInt(line.charAt(left)))) {
-          break;
-        }
-      }
-  
-      // now go right and find the symble
-      for(right; right < line.length; right++) {
-        if (isNaN(parseInt(line.charAt(right)))) {
-          break;
-        }
-      }
-  
-      // parse the number, and only count it if it isn't already accounted for.
-      const number = parseInt(line.substring(left+1, right));
-      if (!existingNumbers.includes(number)) {
-        existingNumbers.push(number);
-        return number;
-      }
-      return 0;
-  
-    }
-    return 0;
+class MultiplicationPairs {
+  public x: number = 0
+  public y: number = 0
+
+  constructor(_x: number, _y: number) {
+    this.x = _x;
+    this.y = _y;
   }
+};
+
+export const findMultiplicationPairs = (input: string): Array<MultiplicationPairs> => {
+  const searchPattern = /mul\((-?\d+),\s*(-?\d+)\)/g;
+  const matches = input.matchAll(searchPattern);
+  const multiplicationPairs = new Array<MultiplicationPairs>();
+  matches.forEach((match) => {
+    multiplicationPairs.push(new MultiplicationPairs(parseInt(match[1]), parseInt(match[2])));
+  });
+  return multiplicationPairs;
+}
+
+export const findMultiplicationPairsFollowsByDo = (input: string): Array<MultiplicationPairs> => {
+  const actionPattern = /do\(\)|don't\(\)/g;
+  const multPattern = /mul\((-?\d+),\s*(-?\d+)\)/g;
+
+  // Combine and sort matches
+  const matches = [...input.matchAll(actionPattern), ...input.matchAll(multPattern)]
+    .map(match => ({ text: match[0], index: match.index || 0, groups: match }))
+    .sort((a, b) => a.index - b.index);
+
+  const multiplicationPairs: MultiplicationPairs[] = [];
+  let takeNext = true;
+
+  for (const match of matches) {
+    if (match.text === "do()") {
+      takeNext = true;
+    } else if (match.text === "don't()") {
+      takeNext = false;
+    } else if (match.text.startsWith("mul(")) {
+      const [_, x, y] = match.groups!;
+      if (takeNext) {
+        // Take subsequent mul(x, y) only if allowed by 'do()'
+        multiplicationPairs.push(new MultiplicationPairs(parseInt(x), parseInt(y)));
+      }
+    }
+  }
+
+  return multiplicationPairs;
+};
